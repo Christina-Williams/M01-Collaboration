e27c4500dc90e41197e55afcb1068613656b776d
Christina Williams <79855581+Christina-Williams@users.noreply.github.com>
2022-11-01 13:15:44 -0400

    Add files via upload

---
+++ M02_Case_Study.py
@@ -0,0 +1,21 @@
+#christina Williams
+#GPA Congratulator
+#Write a Python app that will accept student names and GPAs and test if the student qualifies for either the Dean's List or the Honor Roll. Your app will:
+#ask for and accept a student's last name.
+#quit processing student records if the last name entered is 'ZZZ'.
+#ask for and accept a student's first name.
+#ask for and accept the student's GPA as a float.
+#test if the student's GPA is 3.5 or greater and, if so, print a message saying that the student has made the Dean's List.
+#test if the student's GPA is 3.25 or greater and, if so, print a message saying that the studnet has made the Honor Roll.
+
+studentFirstName=input(" Enter your first name")
+studentLastName=input(" Enter your last name")
+GPA=float(input ("Enter your GPA"))
+
+if studentLastName ==("ZZZ"): 
+    quit()
+    if GPA >=3.5: 
+        print(" Congratulations, you've made the Dean's List!")
+    if GPA >=3.25:
+        print(" Congratulations, you've made the Honor Roll!")
+
